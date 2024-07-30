a5da94a03a571a7765aea2d1921965a40356e1df
Keke <kekearif@googlemail.com>
2024-07-30 20:30:40 +0800

    feat: add example

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,77 @@
+//
+//  AppDelegate.swift
+//  CBAssistiveTouchExample
+//
+//  Created by yyjim on 2019/8/28.
+//  Copyright © 2019 Cardinalblue. All rights reserved.
+//
+
+import CBAssistiveTouch
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    lazy var window: UIWindow? = {
+        UIWindow(frame: UIScreen.main.bounds)
+    }()
+
+    private lazy var assistiveTouch: AssistiveTouch = {
+        let contentViewController = CBConsoleViewController()
+        contentViewController.toggleHandler = { [unowned self] in
+            self.assistiveTouch.toggleContent()
+        }
+        let layout = DefaultAssistiveTouchLayout(applicationWindow: self.window)
+        if #available(iOS 11, *) {
+            layout.safeAreaInsets = window?.safeAreaInsets ?? .zero
+        }
+        layout.customView = { () -> UIView in
+            let label = UILabel(frame: .zero)
+            label.text = "🛠️"
+            label.sizeToFit()
+            return label
+        }()
+        layout.assistiveTouchSize = layout.customView!.bounds.size
+        layout.margin = 15
+        contentViewController.preferredContentSize = CGSize(width: UIScreen.main.bounds.size.width - 2 * layout.margin, height: 300)
+        return AssistiveTouch(applicationWindow: window, layout: layout, contentViewController: contentViewController)
+    }()
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let rootVC = ViewController(assistiveTouch: assistiveTouch)
+
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+
+        assistiveTouch.show()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
